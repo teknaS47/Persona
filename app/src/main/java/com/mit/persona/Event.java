c3073ae2fd1c459e137ee79f2b023a6d38568e82
ketansingrole <36977396+ketansingrole@users.noreply.github.com>
2018-12-25 21:17:28 +0530

    25-12-18 Event Page made dynamic and created a file pageDetails having values of dynamic

--- app/src/main/java/com/mit/persona/Event.java
+++ app/src/main/java/com/mit/persona/Event.java
@@ -2,6 +2,7 @@ package com.mit.persona;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Event extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class Event extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+        TextView eventDetails = (TextView) findViewById(R.id.textView6);
+        eventDetails.setText(pageDetails.eventDetails);
+        TextView date = (TextView) findViewById(R.id.textView5);
+        date.setText(pageDetails.eventDate);
+
     }
 }
---
+++ app/src/main/java/com/mit/persona/pageDetails.java
@@ -0,0 +1,8 @@
+package com.mit.persona;
+
+public class pageDetails {
+    public static String eventDetails  = "====";
+    public static String eventLikes = "+++++++";
+    public static String eventNmae = "---------";
+    public static String eventDate = "////////";
+}
