b6540b722f1059e2ed2a86f87d04364364969791
teknaS47 <sanketsaikia13@gmail.com>
2018-12-25 23:36:17 +0530

    25.12.18 2335

--- app/src/main/java/com/mit/persona/loginActivity.java
+++ app/src/main/java/com/mit/persona/loginActivity.java
@@ -3,13 +3,16 @@ package com.mit.persona;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class loginActivity extends AppCompatActivity /*implements OnClickListener*/{
@@ -48,6 +51,24 @@ public class loginActivity extends AppCompatActivity /*implements OnClickListene
 
 
     }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent( event );
+    }
+
 
     //private static final String TAG = "loginActivity ###################";
     /*public void onClick(View view) {
