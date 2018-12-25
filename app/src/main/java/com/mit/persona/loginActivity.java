0f324f07d8ea1c5887081e63a361fc2353ff82ad
teknaS47 <sanketsaikia13@gmail.com>
2018-12-25 21:31:11 +0530

    25.12.18 2130

--- app/src/main/java/com/mit/persona/loginActivity.java
+++ app/src/main/java/com/mit/persona/loginActivity.java
@@ -1,15 +1,19 @@
 package com.mit.persona;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class loginActivity extends AppCompatActivity /*implements OnClickListener*/{
@@ -43,11 +47,43 @@ public class loginActivity extends AppCompatActivity /*implements OnClickListene
         skip_bt.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(loginActivity.this, Persona.class));
+
             }
         });
 
+        //hideKeyboard(loginActivity.this);
+
 
     }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent( event );
+    }
+
+    /*public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+*/
 
     //private static final String TAG = "loginActivity ###################";
     /*public void onClick(View view) {
