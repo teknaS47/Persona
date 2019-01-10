d114c9a7e400aa2071454e70230697a8ae2e4ae4
ketansingrole <36977396+ketansingrole@users.noreply.github.com>
2019-01-11 01:15:23 +0530

    FINAL STABLE VERION!

--- app/src/main/java/com/mit/persona/loginActivity.java
+++ app/src/main/java/com/mit/persona/loginActivity.java
@@ -134,6 +134,22 @@ public class loginActivity extends AppCompatActivity /*implements OnClickListene
         }
 
     }
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event){
+        if(event.getAction()==MotionEvent.ACTION_DOWN){
+            View v=getCurrentFocus();
+            if(v instanceof EditText){
+                Rect outRect= new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if(!outRect.contains((int)event.getRawX(),(int)event.getRawY())){
+                    v.clearFocus();
+                    InputMethodManager imm=(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(),0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
+    }
    /* public void gotopersona()
     {
         Intent i = new Intent(mContext,Persona.class);
