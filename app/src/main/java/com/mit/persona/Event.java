82f48931428aaecf86575ce49d1ca3ca6e53716b
ay <akshayy29@gmail.com>
2019-01-23 00:07:45 +0530

    New iamges!

--- app/src/main/java/com/mit/persona/Event.java
+++ app/src/main/java/com/mit/persona/Event.java
@@ -17,28 +17,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.net.Uri;
-
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 
 public class Event extends AppCompatActivity {
 
